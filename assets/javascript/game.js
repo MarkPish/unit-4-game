72937fea395995939fa7fe713176b250740cd99e
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-08-23 17:40:34 -0400

    portfolio connect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,33 @@
 
 // Generate randon mumber.....use a document.ready? 
-$(document).ready(function()
- {
-    let Random=Math.floor(Math.random()*(120-19+1)+19);
+$(document).ready(function(){
+
+let Random=Math.floor(Math.random()*(120-19+1)+19);
 
 
 // Display random number
 $("#scoreToMatch").text(Random);
 
 // Generate random number for each crystal
-    let num1= Math.floor(Math.random()*12+1);
-    let num2= Math.floor(Math.random()*12+1);
-    let num3= Math.floor(Math.random()*12+1);
-    let num4= Math.floor(Math.random()*12+1);
+let num1= Math.floor(Math.random()*12+1);
+let num2= Math.floor(Math.random()*12+1);
+let num3= Math.floor(Math.random()*12+1);
+let num4= Math.floor(Math.random()*12+1);
 
 
 // Create variables to keep track of wins, losses and total score...research .text?
 
-    let playerTotal = 0;
-    let wins = 0;
-    let losses = 0;
+let playerTotal = 0;
+let wins = 0;
+let losses = 0;
 
 $("#wins").text(wins);
 $("#losses").text(losses);
 
-{
+
     // return Math.floor(Math.random()*(max-min+1)+min);
 // Reset the game
-function reset() 
-    {
+function reset() {
     Random=Math.floor(Math.random()*(120-19+1)+19);
     console.log(Random);
     $("#scoreToMatch").text(Random);
@@ -108,8 +107,7 @@ $(".yellow").on("click", function()
         loser();
         }
 })
-$(".green").on("click", function()
-    {
+$(".green").on("click", function(){
     playerTotal = playerTotal + num4;
     console.log("New playerTotal= " + playerTotal);
     $("#totalScore").text(playerTotal);
