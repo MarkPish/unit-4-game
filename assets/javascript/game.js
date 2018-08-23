6170a3e88d1a40dc9b8a9e1d885a86f2704acf11
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-08-23 16:38:37 -0400

    random number changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,17 @@
 // Generate randon mumber.....use a document.ready? 
 $(document).ready(function()
  {
-    let Random=Math.floor(Math.random()*120+19)
+    let Random=Math.floor(Math.random()*(120-19+1)+19);
 
 
 // Display random number
 $("#scoreToMatch").text(Random);
 
 // Generate random number for each crystal
-    let num1= Math.floor(Math.random()*12+1)
-    let num2= Math.floor(Math.random()*12+1)
-    let num3= Math.floor(Math.random()*12+1)
-    let num4= Math.floor(Math.random()*12+1)
+    let num1= Math.floor(Math.random()*12+1);
+    let num2= Math.floor(Math.random()*12+1);
+    let num3= Math.floor(Math.random()*12+1);
+    let num4= Math.floor(Math.random()*12+1);
 
 
 // Create variables to keep track of wins, losses and total score...research .text?
@@ -24,30 +24,105 @@ $("#scoreToMatch").text(Random);
 $("#wins").text(wins);
 $("#losses").text(losses);
 
+{
+    // return Math.floor(Math.random()*(max-min+1)+min);
 // Reset the game
-function reset(){
-    Random=Math.floor(Math.random()*120+19)
-    console.log(Random)
+function reset() 
+    {
+    Random=Math.floor(Math.random()*(120-19+1)+19);
+    console.log(Random);
+    $("#scoreToMatch").text(Random);
+    num1= Math.floor(Math.random()*12+1);
+    num2= Math.floor(Math.random()*12+1);
+    num3= Math.floor(Math.random()*12+1);
+    num4= Math.floor(Math.random()*12+1);
+    playerTotal= 0;
+
+    $("#totalScore").text(playerTotal);
+
+}
+// Show wins
+
+function chixDinner()
+    {
+    alert ("Winner, winner...chicken dinner!");
+        wins++;
+        $("#wins").text(wins);
+        reset();
+    
+    }
+// Show losses
+function loser()
+    {
+    alert ("LOOOSER!!!!");
+        losses++;
+        $("#losses").text(losses);
+        reset()
 }
 
+// Clicking the crystals
+$(".blue").on("click", function()
+    {
+    playerTotal = playerTotal + num1;
+    console.log("New playerTotal= " + playerTotal);
+    $("#totalScore").text(playerTotal);
 
-}
+        // Conditionals for win or lose
+        if (playerTotal == Random)
+        {
+        chixDinner();       
+        }
+        else if (playerTotal > Random)
+        {
+        loser();
+        }
+})
+$(".red").on("click", function()
+    {
+    playerTotal = playerTotal + num2;
+    console.log("New playerTotal= " + playerTotal);
+    $("#totalScore").text(playerTotal);
 
+        // Conditionals for win or lose
+        if (playerTotal == Random) {
+        chixDinner();       
+        }
+        else if (playerTotal > Random)
+        {
+        loser();
+        }
+})
+$(".yellow").on("click", function()
+    {
+    playerTotal = playerTotal + num3;
+    console.log("New playerTotal= " + playerTotal);
+    $("#totalScore").text(playerTotal);
 
+        // Conditionals for win or lose
+        if (playerTotal == Random) 
+        {
+        chixDinner();       
+        }
+        else if (playerTotal > Random)
+        {
+        loser();
+        }
+})
+$(".green").on("click", function()
+    {
+    playerTotal = playerTotal + num4;
+    console.log("New playerTotal= " + playerTotal);
+    $("#totalScore").text(playerTotal);
 
+        // Conditionals for win or lose
+        if (playerTotal == Random) 
+        {
+        chixDinner();       
+        }
+        else if (playerTotal > Random)
+        {
+        loser();
+        }
+    })
 
-// Display wins
-
-
-
-// Display losses
-
-
-
-
-// Create buttons for the crystal images
-
-
-
-
-// Clicking crystals
+});
